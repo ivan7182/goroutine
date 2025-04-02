572f7e28b6f4a1d385b8526975f58068ea2c5ed2
ivan7182 <muhammadivan7182@gmail.com>
2025-04-02 08:27:30 +0700

    goroutine

---
+++ channel_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "ivan satria"
+		fmt.Println("selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+// channel parameter
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "ivan satria"
+}
+
+func TestChannelParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}

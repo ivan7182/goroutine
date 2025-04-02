6b79209d0254756c688936da4baddfda98833de4
ivan7182 <muhammadivan7182@gmail.com>
2025-04-02 14:30:42 +0700

    goroutine

--- channel_test.go
+++ channel_test.go
@@ -39,3 +39,50 @@ func TestChannelParameter(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+// In Out Channel
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "ivan satria"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
+
+// Buffered Channel
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+
+		channel <- "Ivan"
+		channel <- "satria"
+		channel <- "joko"
+
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+
+	}()
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("selesai")
+
+}

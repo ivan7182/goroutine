f2f3765d79d34bca80ce0ac53c2e87faa160e63c
ivan7182 <muhammadivan7182@gmail.com>
2025-04-04 10:33:18 +0700

    goroutine

--- mutex_test.go
+++ mutex_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestMutex(t *testing.T) {
@@ -31,4 +32,37 @@ func TestMutex(t *testing.T) {
 	if x != expected {
 		t.Errorf("Expected counter to be %d, but got %d", expected, x)
 	}
+}
+
+type BankAccount struct {
+	RwMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RwMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RwMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RwMutex.RLock()
+	balancee := account.Balance
+	account.RwMutex.RUnlock()
+	return balancee
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total balance ", account.GetBalance())
 }

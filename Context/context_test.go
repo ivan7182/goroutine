0509394dcaa152b08acb455cf6cca76dac3d4ce2
ivan7182 <muhammadivan7182@gmail.com>
2025-04-07 14:49:55 +0700

    'Context'

--- Context/context_test.go
+++ Context/context_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -12,4 +14,68 @@ func TestContext(t *testing.T) {
 
 	todo := context.TODO()
 	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+	contextG := context.WithValue(contextF, "g", "G")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+	fmt.Println(contextG)
+
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b"))
+
+}
+
+// context with cancel
+func CreateDestination(ctx context.Context) chan int {
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateDestination(ctx)
+	for n := range destination {
+		fmt.Println("counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel()
+	time.Sleep(5 * time.Second)
+	fmt.Println("total goroutine", runtime.NumGoroutine())
 }
